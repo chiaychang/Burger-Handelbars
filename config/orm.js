4803e647b57e41f9cdbae9935e524309f0bf2353
Joy Chang <joychang@Joyteki-MacBook-Pro.local>
2017-05-13 08:35:36 -0500

    adding semi functional app

---
+++ config/orm.js
@@ -0,0 +1,41 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+
+    selectAll: function(tableInput, cb) {
+
+        connection.query("Select * from " + tableInput, function(err, result) {
+
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+
+    },
+
+    insertOne: function(table, cols, vals, cb) {
+        connection.query("Insert into " + table + " (" + cols + ") " + "VALUES (?,?)", vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+
+    updateOne: function(table, condition, cb) {
+        connection.query("UPDATE " + table + " SET devoured=true WHERE " + devourPick, function(err, result) { 
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+
+    }
+
+
+}
+
+
+module.exports = orm;
